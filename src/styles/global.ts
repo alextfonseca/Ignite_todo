654ac93b0d8cd68ad1f8b7774dfda6c88ae79cc7
Alex Teixeira da Fonseca <alexatf2014@gmail.com>
2022-09-16 20:33:30 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme["purple-200"]};
+  }
+
+  body{
+    background: ${(props) => props.theme["gray-600"]};
+    color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  } 
+`;
